33fb4402c1abb295e3be3f46f0f4970274f3f232
Caio Azevedo <azevedodomingues01@gmail.com>
2024-06-12 17:35:16 -0300

    container

--- Script/home.js
+++ Script/home.js
@@ -1,15 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    verificarAutenticacao();
-});
-
-
-const verificarAutenticacao = () => {
-    const autorizado = localStorage.getItem("autorizado");
-    if(autorizado !== "true"){
-        alert("Usuario não cadastrado, Efetue o login e tente novamente!");
-        window.location = './index.html';
-    }
-}
 
 
 const selectOp = () => {
